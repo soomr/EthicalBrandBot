2afa622aa8287186d590498f1b87fbe642af06b8
soomr <52680308+soomr@users.noreply.github.com>
2021-09-06 16:20:21 -0400

    WIP

---
+++ main.py
@@ -0,0 +1,8 @@
+import praw
+
+reddit = praw.Reddit(client_id = "S25TqSFtRNSUUQ",
+client_secret="	zIemYqLjBK64mmmfVSljwC9fiSw",
+    password = "" , user_agent = "Ethical Brand Bot",
+    username = ""
+
+)
